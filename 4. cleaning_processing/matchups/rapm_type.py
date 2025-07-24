c2671dae828d2f9d47bee600bf3eed998c9b8b0d
Spencer Thompson <spencer_thompson1@baylor.edu>
2025-07-23 22:40:11 -0700

    done

--- 4. cleaning_processing/matchups/rapm_type.py
+++ 4. cleaning_processing/matchups/rapm_type.py
@@ -5,4 +5,8 @@ import pandas as pd
 RAW_DATA_DIR    = '3. raw_data'
 MATCHUPS_DIR    = os.path.join(RAW_DATA_DIR, 'matchups')
 
-#
+# Read all CSV files in the MATCHUPS_DIR that start with "matchups_"
+csv_files = [f for f in os.listdir(MATCHUPS_DIR) if f.startswith("matchups_") and f.endswith(".csv")]
+
+# Combine them into one DataFrame
+master_df = pd.concat([pd.read_csv(os.path.join(MATCHUPS_DIR, file)) for file in csv_files], ignore_index=True)
